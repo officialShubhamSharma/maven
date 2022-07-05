96da7940651552b50c4e6eacda800655ba83d13b
“Shubham” <officialshubhamsharma9222@gmail.com>
2022-07-05 17:11:40 +0000

    first

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -6,7 +6,7 @@ package com.mycompany.app;
 public class App
 {
 
-    private final String message = "Jenkins->GIT->Maven";
+    private final String message = "Hello World!";
 
     public App() {}
 
